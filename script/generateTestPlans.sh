5802cb170c32977543e8ead14757de939dab4342
Tuan Hiep <tuanhiep.tran88@gmail.com>
2014-04-17 10:52:59 +0200

    modify script generating

--- script/generateTestPlans.sh
+++ script/generateTestPlans.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
 ANUMCLIENT=( 100 1000 10000)
-AQOS=(mqtt_at_most_once mqtt_at_least_once mqtt_extactly_once)
+AQOS=(0 1 3)
 ARETAIN=(true false)
-
+qos[0]=mqtt_at_most_once
+qos[1]=mqtt_at_least_once
+qos[3]=mqtt_extactly_once
+if [ ! -d "TestPlans" ]; then
+  mkdir TestPlans
+fi
 for NUMCLIENT in ${ANUMCLIENT[*]}
 do
 for QOS in ${AQOS[*]}
@@ -12,7 +17,7 @@ for RETAIN in ${ARETAIN[*]}
 do
         SUFFIX=$NUMCLIENT.$QOS.$RETAIN
 	echo "generating testplan.$SUFFIX.jmx"
-	sed  's/__NUMCLIENT__/'$NUMCLIENT'/g' TestPlan.tmpl.jmx | sed 's/__QOS__/'$QOS'/g'  | sed 's/__RETAIN__/'$RETAIN'/g' > testplan.$SUFFIX.jmx
+	sed  's/__NUMCLIENT__/'$NUMCLIENT'/g' TestPlan.tmpl.jmx | sed 's/__QOS__/'${qos[$QOS]}'/g'  | sed 's/__RETAIN__/'$RETAIN'/g' >TestPlans/testplan.$SUFFIX.jmx
 done
 done
 done
